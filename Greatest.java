39be8d7b40143f290f1873f389eaf12548c3df75
LazieCod <shivajsingh2006@gmail.com>
2026-05-12 06:19:02 +0530

    Added Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Greatest {
+
+    static int greatest(int a, int b, int c) {
+
+        int max = a;
+
+        if(b > max) {
+            max = b;
+        }
+
+        if(c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        int result = greatest(a, b, c);
+
+        System.out.println("Greatest number is: " + result);
+    }
+}
